551d4758e63e64740e8bdb8deac34d0b09003cfc
Maanav Rajesh <maanav.rajesh@botpress.com>
2025-11-26 13:36:22 -0500

    feat: almost done botpress bot ui

---
+++ botpress-web-tester/app/page.tsx
@@ -0,0 +1,123 @@
+'use client'
+
+import {
+  Container,
+  Header,
+  MessageList,
+  Composer,
+  useWebchat,
+  Fab,
+} from "@botpress/webchat";
+import { useState, useMemo } from "react";
+import "./webchat-styles.css";
+
+const headerConfig = {
+  botName: "Botpress",
+  botAvatar: "https://cdn.botpress.cloud/bot-avatar.png",
+
+  phone: {
+    title: "Call Support",
+    link: "tel:+1234567890",
+  },
+
+  email: {
+    title: "Email Us",
+    link: "mailto:support@example.com",
+  },
+
+  website: {
+    title: "Visit our website",
+    link: "https://www.example.com",
+  },
+
+  termsOfService: {
+    title: "Terms of Service",
+    link: "https://www.example.com/terms",
+  },
+
+  privacyPolicy: {
+    title: "Privacy Policy",
+    link: "https://www.example.com/privacy",
+  },
+};
+
+export default function Home() {
+  const [isWebchatOpen, setIsWebchatOpen] = useState(true);
+  const { client, messages, isTyping, user, clientState, newConversation } =
+    useWebchat({
+      clientId: "2f5a29ff-c8c2-43c2-94cb-afa173526f7a", // Insert your Client ID here
+    });
+
+  const config = {
+    botName: "SupportBot",
+    botAvatar: "https://picsum.photos/id/80/400",
+    botDescription: "Your virtual assistant for all things support.",
+  };
+  const enrichedMessages = useMemo(
+    () =>
+      messages.map((message) => {
+        const { authorId } = message;
+        const direction = authorId === user?.userId ? "outgoing" : "incoming";
+        return {
+          ...message,
+          direction,
+          sender:
+            direction === "outgoing"
+              ? { name: user?.name ?? "You", avatar: user?.pictureUrl }
+              : { name: config.botName ?? "Bot", avatar: config.botAvatar },
+        };
+      }),
+    [
+      config.botAvatar,
+      config.botName,
+      messages,
+      user?.userId,
+      user?.name,
+      user?.pictureUrl,
+    ]
+  );
+
+  const toggleWebchat = () => {
+    setIsWebchatOpen((prevState) => !prevState);
+  };
+
+  return (
+    <>
+      <Container
+        connected={clientState !== "disconnected"}
+        style={{ display: isWebchatOpen ? "flex" : "none" }}
+      >
+        <Header
+          // onOpenChange={() => console.log('Override the header open change')}
+          defaultOpen={false}
+          closeWindow={() => setIsWebchatOpen(false)}
+          restartConversation={newConversation}
+          disabled={false}
+          configuration={headerConfig}
+        />
+        <MessageList
+          // botAvatar={config.botAvatar}
+          botName={config.botName}
+          botDescription={config.botDescription}
+          isTyping={isTyping}
+          headerMessage="Chat History"
+          showMarquee={true}
+          messages={enrichedMessages}
+          sendMessage={client?.sendMessage}
+        />
+        <Composer
+          disableComposer={false}
+          isReadOnly={false}
+          allowFileUpload={true}
+          connected={clientState !== "disconnected"}
+          sendMessage={client?.sendMessage}
+          uploadFile={client?.uploadFile}
+          composerPlaceholder="Type a message..."
+        />
+      </Container>
+      <Fab
+        onClick={() => toggleWebchat()}
+      />
+    </>
+  );
+}
